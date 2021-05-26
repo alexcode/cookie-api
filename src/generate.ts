d62f119f513febf5b7af54c4dc6b8644bfaf9346
Alex Couturon <infocontact.alex@gmail.com>
2021-05-26 17:45:42 +0200

    Create api folder at generate time

--- src/generate.ts
+++ src/generate.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import path from 'path';
 import fs from 'fs';
 import { JsonDB } from 'node-json-db';
 import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
@@ -9,11 +10,14 @@ program.parse();
 
 const DB = new JsonDB(new Config("db/cookiedata", false, true));
 const list: Array<CookieItem> = Object.values(DB.getData('/'));
+const dir = path.resolve('./api');
+
+fs.mkdirSync(dir, { recursive: true });
+
 const files = list.map((cookie: CookieItem) => fs.promises.writeFile(
-    `${__dirname}/../api/${cookie.key}.json`, 
+    `${dir}/${cookie.key}.json`, 
     JSON.stringify(cookie), 
     { encoding: 'utf8', flag: 'w' }
 ));
+
 Promise.all(files).then(console.log).catch(console.error);
-
-
